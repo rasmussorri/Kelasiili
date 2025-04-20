d079ee3d4364b23b725efe73d317edd467fc9cee
rasmussorri <rasmus.sorri@gmail.com>
2025-04-20 19:21:08 +0300

    ApiServiceBuilder ei oo enää singleton

--- app/src/main/java/com/example/myapplication/utilities_plus_helpers/ApiServiceBuilder.java
+++ app/src/main/java/com/example/myapplication/utilities_plus_helpers/ApiServiceBuilder.java
@@ -4,24 +4,15 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiServiceBuilder {
-    private static Retrofit retrofit = null;
-
-    // Luo singleton retrofit instanssin, jolle annetaan APIn verkko-osoite
+    // Luo aina uusi Retrofit-instanssi annetulla URL:lla
     // Muuntaja muuntaa haetun JSON-muotoisen datan Java-olioiksi
-    // Nyt voidaan luoda useita API palveluja samalla metodilla
-    public static Retrofit getClient(String url) {
-        if (retrofit == null) {
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(url)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-        return retrofit;
-    }
-
     // Käytetään geneeristä metodia, joka luo API palvelun
-    public static <T> T createService(Class<T> serviceClass, String url) {
-        return getClient(url).create(serviceClass);
-    }
+    public static <T> T createService(Class<T> serviceClass, String baseUrl) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl.endsWith("/") ? baseUrl : baseUrl + "/") // Varmista lopussa '/'
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        return retrofit.create(serviceClass);
+    }
 }
